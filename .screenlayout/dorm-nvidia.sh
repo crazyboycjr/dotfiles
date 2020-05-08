12182a80c8861bfb808af52a4dd9b040b464a382
Jingrong Chen <crazyboycjr@gmail.com>
2020-05-08 21:21:51 +0800

    add screenlayout

---
+++ .screenlayout/dorm-nvidia.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --setprovideroutputsource modesetting NVIDIA-0
+xrandr --output eDP-1-1 --mode 1920x1080 --pos 3840x1080 --rotate normal --output DP-1-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1-1 --off --output DP-1-2 --off --output HDMI-1-2 --off
