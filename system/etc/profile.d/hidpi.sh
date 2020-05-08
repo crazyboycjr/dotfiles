a5e07dce6439e6f0977ef467c579f53d40962235
Jingrong Chen <crazyboycjr@gmail.com>
2020-05-08 21:26:19 +0800

    add configs in system profile.d and bashrc

---
+++ system/etc/profile.d/hidpi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export QT_AUTO_SCREEN_SCALE_FACTOR=2
+#export QT_SCALE_FACTOR=2
+#export GDK_SCALE=2
---
+++ system/etc/profile.d/nix.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+if [ -e /home/cjr/.nix-profile/etc/profile.d/nix.sh ]; then . /home/cjr/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
